fd98f78b4d917e24c7563e5a2ce0d671cfbc0613
Aayush Twayana <aayush.twayana@gmail.com>
2020-01-13 20:02:27 +0545

    addded templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Hello World!</h1>')
+    # return HttpResponse('<h1>Hello World!</h1>')
+    return render(request, 'blog/home.html')
 
 def about(request):
     return HttpResponse('<h1>Hello About!</h1>')
