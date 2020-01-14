92c5aab7bcb11eacb06aded09b4440f367b58a8c
Aayush Twayana <aayush.twayana@gmail.com>
2020-01-14 09:31:47 +0545

    added some bunch of templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render     #render also returns an HttpResponse at the back
+# from django.http import HttpResponse
+
+#dummy post datas
+posts = [
+    {
+        'title': 'Post 1',
+        'author': 'Aayush twayana',
+        'date_posted': '27th march, 2000',
+        'content': 'anything'
+    },
+    {
+        'title': 'Post 2',
+        'author': 'Prayush twayana',
+        'date_posted': '27th july, 2004',  
+        'content': 'anything'
+    }
+]
+
 
 def home(request):
     # return HttpResponse('<h1>Hello World!</h1>')
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Hello About!</h1>')
+    # return HttpResponse('<h1>Hello About!</h1>')
+    return render(request, 'blog/about.html', {'title': 'about'})
