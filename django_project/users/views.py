effa7b237e008704feda989107164da2f0f2c9a7
Aayush Twayana <aayush.twayana@gmail.com>
2020-01-15 09:00:17 +0545

    added user register views and user register templates

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register(request):
+    form = UserCreationForm()       #creating an instance of UserCreationForm Class
+    return render(request, users/register.html, {'form': form})       #oassing a dictionary as the context
